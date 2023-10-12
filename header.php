33e38d72d439a44a9bc4b1cc33da96d4bbbceec1
Jacob <sayaomarkisrael@gmail.com>
2023-10-12 15:37:38 +0800

    elementor

---
+++ header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <?php wp_head(); ?>
+</head>
+<body>
+    
+<!-- <header class="header">
+    <div class="container">
+        <div class="header_wrapper">
+            <div class="logo">
+                One Piece
+            </div>
+            <nav>
+                <ul>
+                    <li>Luffy</li>
+                    <li>Zoro</li>
+                    <li>Sanji</li></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</header> -->
---
+++ includes/assets.php
@@ -0,0 +1,7 @@
+<?php
+
+function theblog_assets() {
+    wp_enqueue_style('theBlog-style', get_template_directory_uri() . './css/main.css' , microtime());
+}
+
+add_action('wp_enqueue_scripts', 'theblog_assets');
